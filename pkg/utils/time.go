11cf11930a3a9784e92cbe13306ae142ef74f8c3
DylanSatow <dylansatow531@gmail.com>
2025-07-19 19:24:59 -0700

    Time parsing for log

---
+++ pkg/utils/time.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// ParseTimeframe parses timeframe strings like "1h", "2h", "today", "30m"
+func ParseTimeframe(timeframe string) (time.Time, error) {
+	switch strings.ToLower(timeframe) {
+	case "today":
+		return time.Now().Truncate(24 * time.Hour), nil
+	case "yesterday":
+		return time.Now().Add(-24 * time.Hour).Truncate(24 * time.Hour), nil
+	}
+
+	// Parse duration format like "1h", "30m", "2h30m"
+	if strings.HasSuffix(timeframe, "h") || strings.HasSuffix(timeframe, "m") {
+		duration, err := time.ParseDuration(timeframe)
+		if err != nil {
+			return time.Time{}, fmt.Errorf("invalid timeframe format: %s", timeframe)
+		}
+		return time.Now().Add(-duration), nil
+	}
+
+	// Try to parse as number of hours
+	if hours, err := strconv.Atoi(timeframe); err == nil {
+		return time.Now().Add(-time.Duration(hours) * time.Hour), nil
+	}
+
+	return time.Time{}, fmt.Errorf("unsupported timeframe format: %s", timeframe)
+}
+
+// FormatTimeRange creates a human-readable time range string
+func FormatTimeRange(since time.Time) string {
+	now := time.Now()
+	duration := now.Sub(since)
+
+	if duration < time.Hour {
+		minutes := int(duration.Minutes())
+		return fmt.Sprintf("%s - %s (%dm)", since.Format("15:04"), now.Format("15:04"), minutes)
+	}
+
+	if duration < 24*time.Hour && since.Day() == now.Day() {
+		return fmt.Sprintf("%s - %s", since.Format("15:04"), now.Format("15:04"))
+	}
+
+	if since.Day() == now.Day() {
+		return fmt.Sprintf("Today %s - %s", since.Format("15:04"), now.Format("15:04"))
+	}
+
+	return fmt.Sprintf("%s - %s", since.Format("Jan 2 15:04"), now.Format("Jan 2 15:04"))
+}
